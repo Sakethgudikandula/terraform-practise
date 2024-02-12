6ce7777ae0f8732308d4d9d122506bceabc8d05c
Sakethgudikandula <140391648+Sakethgudikandula@users.noreply.github.com>
2024-02-12 21:13:01 +0530

    change done

--- Day 1/modules/ec2_instance/main.tf
+++ Day 1/modules/ec2_instance/main.tf
@@ -1,13 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ""
-<<<<<<< HEAD
-    secret_key = 
-=======
-    secret_key = "" 
->>>>>>> 441893d6cf6c697a8972dd2019b8f9ee2e56957b
+    access_key = "
 }
-
 resource "aws_instance" "practise" {
     ami = var.ami
     instance_type = var.instance_type
